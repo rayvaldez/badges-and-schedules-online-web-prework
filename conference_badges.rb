a84d884f8cb3f459eb5c311d9ef6d378830e51eb
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-13 14:43:14 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,14 @@ def badge_maker(name)
   name = "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
-
+def batch_badge_creator(badge)
+  counter = 0
+  attendees = []
+  badge.each do |badge|
+    attendees[0] badge_maker(name)
+    counter += 1
+  end
+  attendees
 end
 
 def assign_rooms
